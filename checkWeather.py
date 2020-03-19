f74b889ae0fbe9675cbeaee84d75b48e7ad48aa2
cinun <43857696+cinun@users.noreply.github.com>
2020-03-18 21:08:11 -0500

    Update checkWeather.py

--- checkWeather.py
+++ checkWeather.py
@@ -1,10 +1,9 @@
 import requests, json
 from sys import argv
 
-
 #Declaring Constants
-KEY = "e0b1a3f7e2a349c5da6ae97ed5cdbd74"
 weather_dict = {"Rain":"Rainy", "Clouds":"Cloudy", "Clear":"Sunny & Clear", "Sand":"Sand Stormy ","Drizzle":"Drizzling"}
+
 
 def get_weather(KEY, location):
     openweather = ("http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}".format(location, KEY))
@@ -14,7 +13,7 @@ def get_weather(KEY, location):
 
 def main():
     location = ""
-    KEY = "e0b1a3f7e2a349c5da6ae97ed5cdbd74"
+    KEY = "Get key from openweather api "
     weather_dict = {"Rain":"Rainy", "Clouds":"Cloudy", "Clear":"Sunny & Clear", "Sand":"Sand Stormy ","Drizzle":"Drizzling"}
     for i in range(1, len(argv)):
         location += argv[i]
@@ -27,10 +26,11 @@ def main():
 def printWeather(weather, weather_dict, location):
     try:
         sky = weather_dict[weather['weather'][0]['main']]
-        temp = weather['main']['temp']
-        print("In {} it is currently {} with a temperature of {} F".format(location, sky, temp))
+        temp = weather['main']['temp']*(9/5) - 459.67
+
+        return ('In {} it is currently {} with a temperature of {:.2f} F'.format(location, sky, temp))
     except KeyError:
-        print("Sorry I don't have the data for {}".format(location))
+        return ("Sorry I don't have the data for {}".format(location))
 
 
 if __name__ == "__main__":
