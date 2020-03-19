2d1ecb13c9e713fae50a059dabd4530a778d9309
cinun <43857696+cinun@users.noreply.github.com>
2020-03-18 20:56:48 -0500

    Create jamesjokes.py

---
+++ jamesjokes.py
@@ -0,0 +1,13 @@
+##Create a file that tells a random joke
+
+import requests, json
+
+
+def joke():
+    y = requests.get('https://official-joke-api.appspot.com/random_joke')
+    joke_dict = json.loads(y.text)
+    joke = joke_dict['setup'] + " " + joke_dict['punchline']
+    #print(joke)
+    return joke
+
+joke()
