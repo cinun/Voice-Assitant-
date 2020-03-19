7f9569985c61d95141f1a0e20c255af4c7033c3d
cinun <43857696+cinun@users.noreply.github.com>
2020-03-18 21:02:06 -0500

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -1,10 +1,3 @@
-#Spotify Client ID: 85cd966e836c411cae26ad9a82ee04c1
-#Spotify Client Secret: 83b8bb9e5d0f4ad2be504f2a5523cf04
-#https://open.spotify.com/user/zhdn6i1w5o89ysl3lx8fcjb2t?si=5qAY6iFrQXuv6K8CtFA_ww
-#Access tokem :BQCC7nLsCj54SWOgG2sClHxxldw8a7WXuXrKkg-Aow_8mn7eqcFeNYattDvZ2wPeYhpF-HZnhbChvsRdxBBtmN4kkLJy-TT2FRSg_ip2mmOrIn_7
-#-e9WC7k30_8nKjljzzmpUUcAoax2M_r3j-S39OFLyi8r-Wez2IWRc-iIWySfFuv5OOyYiUQ
-
-
 import spotipy, os, json
 import spotipy.util as util 
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -12,12 +5,12 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 #Setting environment variables
 
-os.environ["SPOTIPY_CLIENT_ID"] = "85cd966e836c411cae26ad9a82ee04c1"
-os.environ["SPOTIPY_CLIENT_SECRET"] = "83b8bb9e5d0f4ad2be504f2a5523cf04"
+os.environ["SPOTIPY_CLIENT_ID"] = "Get from spotify account"
+os.environ["SPOTIPY_CLIENT_SECRET"] = "Get from spotify account"
 os.environ["SPOTIPY_REDIRECT_URI"] = "https://google.com/"
 
 scope = "user-read-private user-read-playback-state user-modify-playback-state" 
-username = "Cinun"
+username = "UserName"
 
 try:
     token = util.prompt_for_user_token(username, scope)
@@ -43,7 +36,7 @@ def spot(userArtist, userSong):
             if (device["devices"][i]["is_active"])== True:
 
                 deviceID = device["devices"][i]["id"]
-        #print(deviceID)
+        
 
         artistID = spotifyObj.search(userArtist, None, 0, "artist")
 
@@ -72,11 +65,11 @@ def spot(userArtist, userSong):
             songs = songs["items"]
             for every_song in songs:
                 userSong = userSong.lower()
-                userSong = changeWord(userSong)
+                userSong = removePunctuation(userSong)
                 
                 temp_change = every_song["name"]
                 temp_change = temp_change.lower()
-                temp_change = changeWord(temp_change)
+                temp_change = removePunctuation(temp_change)
                 if (temp_change == userSong):
                     #start play back
                     trackUri = []
@@ -90,13 +83,13 @@ def spot(userArtist, userSong):
 
 
 
-def changeWord(initWord):
+def removePunctuation(initWord):
     punctuations = ' !@#$%^&*()_-+=~`[]{};\':",.<>/?\|'
     changed = ""
     for every_char in initWord:
         if every_char not in punctuations:
             changed += every_char
-        elif every_char =='(':
+        elif every_char =='(':          #Ignore anything after the name of the song "(Ft. XYZ)"
             return changed
     return changed
 
@@ -105,11 +98,5 @@ def get_input(someLine):
     tempSplit = someLine.split(",")
     artistName = tempSplit[0].strip()
     songName = tempSplit[1].strip()
-    ##Send this to the function
-    #print(artistName, songName)
     spot(artistName, songName)
-
-#get_input("Eminem, Godzilla")
-
-#spotifyObj.pause_playback()
 
